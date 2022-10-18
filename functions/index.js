b8041234e977d47e17584b14b051a5b318e41267
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-10-18 17:13:44 -0400

    added patch

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,20 @@
 import functions from "firebase-functions";
 import express from 'express';
-import { createUser } from "./src/users.js";
+import { createUser, getAllUsers, updateUser } from "./src/users.js";
 
 const app = express();
+app.use(express.json());
 
 app.get('/test', (req,res) => {
     res.send('Our first cloud API works!');
 })
 
+app.get('/users', getAllUsers);
+
 app.post('/users', createUser);
+
+//To update, the colon is to accept variable user id.
+app.patch('/users/:uid', updateUser);
 
 export const api = functions.https.onRequest(app);
 
--- functions/src/users.js
+++ functions/src/users.js
@@ -4,5 +4,31 @@ export async function createUser(req, res) {
     const db = dbConnect();
     const doc = await db.collection('users').add(req.body)
         .catch(err => res.status(500).send({ succes: false, message: err }));
-    res.send(201).send({ success: true, message: 'User created: ' + doc.id })
+    res.send(201).send({ 
+            success: true, 
+            message: 'User created: ' + doc.id 
+        })
+}
+
+export async function getAllUsers(req,res) {
+    const db = dbConnect();
+    const collection = await db.collection('users').get()
+    .catch(err => res.status(500).send({ succes: false, message: err }))
+    const users = collection.docs.map(doc => {
+        //creates user and adds id to array
+        let user = doc.data();
+        user.uid = doc.id;
+        return user;
+    });
+    res.status(201).send(users);
+}
+
+export async function updateUser(req,res){
+    const { uid } = req.params;
+
+    const db = dbConnect();
+
+    const doc = await db.collection('users').doc(uid).update(req.body)
+        .catch(err => res.status(500).send({ succes: false, message: err }))
+        res.status(201).send(doc);
 }
